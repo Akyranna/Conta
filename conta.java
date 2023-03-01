07da755961ca2626716e8dcb849ae5eff8bfc137
Eduarda B <119666356+Akyranna@users.noreply.github.com>
2023-03-01 10:00:10 -0300

    Update conta.java

--- conta.java
+++ conta.java
@@ -1,21 +1,35 @@
 public class conta {
-    private int numero;
-    private double saldo;
-    private double limite;
-    private String nome;
+        public int numero;
+        public double saldo;
+        public double limite;
+        public String nome;
 
-    private boolean saca (double valor) {
-        if (valor <= this.limite) {
-            if (valor <= saldo) {
-                this.saldo = this.saldo - valor;
-                return true;
+        public boolean saco(double valor){
+            if(valor <= this.limite){
+                if(valor <= saldo){
+                    this.saldo = this.saldo - valor;
+                    System.out.println("Voce sacou " + valor + " reais.\nSaldo atual: " + this.saldo);
+                    return true;
+                }else{
+                    System.out.printf("Você não possui esse montante\nSaldo atual: " + this.saldo);
+                }
+            }else {
+                System.out.println("Acima do limite permitido.");
             }
-        }return false;
-    }
-    private void deposita (double valor){
+            return false;
+        }
+        public void deposita(double valor){
+            this.saldo = this.saldo + valor;
+            System.out.println("Saldo atual: " + this.saldo + valor );
+        }
+        public void transfere(conta destino, double valor){
+            if(valor <= this.saldo){
+                saco(valor);
+                destino.deposita(valor);
+                System.out.println("Transferido " + valor + " reais");
+            }else{
+                System.out.println("Você não possui esse montante");
 
-    }
-    private void transfere (conta destino, double valor){
-
+        }
     }
 }
